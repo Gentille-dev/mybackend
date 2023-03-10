4b7d40b3bc4cff8b7d17fd8a3172c9b767899637
Gentille-dev <ernegentille@gmail.com>
2023-03-10 13:51:50 +0200

    add new modified serverjs get(/)  and swagger

---
+++ server.js
@@ -0,0 +1,69 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import allRoutes from "./routes/allRoutes.js"
+import router from "./routes/blogRoute.js";
+import swaggerDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+
+mongoose.set('strictQuery', false);
+
+// configuring dotenv
+dotenv.config();
+const swaggerDocument = require("../swagger.json");
+
+
+// create server instance
+const app = express();
+
+// use of cors and body parse.....
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+
+//route
+app.get("/", (req, res) => {
+  res.status(200).send(`
+  <h1 style="text-align: center; color: blue; margin-top: 20vh">Welcome to our api homepage</h1>
+  `);
+});
+
+
+
+// app.get("/test", (req, res) => {
+//   res.status(200).send(`
+//   <h1 style="text-align: center; color: blue; margin-top: 20vh">Welcome to our api home page</h1>
+//   `);
+// });
+
+app.use("/api/v1", allRoutes);
+
+// define some variables......
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+// database connection instance
+const con = () => mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  
+});
+
+
+// instance to listen to our server // port
+const startServer = () => app.listen(port);
+
+Promise.all([con(), startServer()])
+  .then(() => {
+    console.log(`MongoDB connected and server listening at http://${host}:${port}`);
+  })
+  .catch((err) => console.log(err))
+
+
+
+  
